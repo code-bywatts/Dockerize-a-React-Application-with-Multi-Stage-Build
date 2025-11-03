398afa803258455d504c80d5e9902ab34f1d1062
Vasu Watts <vasuwattsuser@gmail.com>
2025-11-03 14:25:47 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This React app is served via Nginx in a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
